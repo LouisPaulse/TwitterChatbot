7fae500a2322aecae6fd4f39e764cb31e83a133f
Louis <louispaulse07@gmail.com>
2021-05-19 00:25:03 +0200

    Added interaction support to twitter api on needed functionalities

--- twitter_api.py
+++ twitter_api.py
@@ -1,26 +1,51 @@
-import twitter
+import tweepy
 from api_keys import api_key, api_secret_key, bearer_Token, access_token, access_token_secret
 
-api = twitter.Api(consumer_key=api_key,
-                  consumer_secret=api_secret_key,
-                  access_token_key=access_token,
-                  access_token_secret=access_token_secret,)
+auth = tweepy.OAuthHandler(api_key, api_secret_key)
+auth.set_access_token(access_token, access_token_secret)
 
-users = api.GetUser(screen_name="louis_paulse")
-print(users)
+api = tweepy.API(auth)
 
 
-def get_direct_messages():
-    users = api.GetDirectMessages(since_id=0, count=200)
-    print(users)
+def get_user_info(user_id):
+    """ retrieves basic user information, to be saved in database"""
+    user = api.get_user(user_id=user_id)
+    name = user.name
+    screen_name = user.screen_name
+    location = user.location
+    return name, screen_name, location
 
 
-def send_direct_message(user):
-    pass
+def list_direct_messages_to_be_stored():
+    """ Method retrieves all direct messages using the api and returns a list of objects to be stored in database"""
+    messages = api.list_direct_messages()
+    information_to_be_stored = []
+    for message in messages:
+        user_id = message.message_create["sender_id"]
+        recipient_id = message.message_create["target"]["recipient_id"]
+
+        user_name, user_screen_name, location = get_user_info(user_id)
+
+        message_text = message.message_create["message_data"]
+        time_stamp = message.created_timestamp
+        message_answered = False
+
+        to_be_stored = {
+            "user_id": user_id,
+            "user_name": user_name,
+            "user_screen_name": user_screen_name,
+            "location": location,
+            "message_id": message.id,
+            "message_data": message_text,
+            "recipient_id": recipient_id,
+            "time_stamp": time_stamp,
+            "message_answered": message_answered,
+        }
+        information_to_be_stored.append(to_be_stored)
+
+    return information_to_be_stored
 
 
-def log_message_in_database():
-    pass
+def send_direct_message(user_id, message):
+    api.send_direct_message(recipient_id=user_id, text=message)
 
-
-get_direct_messages()
